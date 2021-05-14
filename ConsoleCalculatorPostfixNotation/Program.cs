306e40c7a0e4107255376ea88bb57c5dd2ac8fed
OrlySedl <orlysedl@gmail.com>
2021-05-14 12:06:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleCalculatorPostfixNotation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleCalculatorPostfixNotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
